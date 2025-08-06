b92bd9758ba238533d8b22b47288d37b3469b58c
7682829 <arunimamehta01325@gmail.com>
2025-08-07 01:03:03 +0530

    Added the hamburger menu

--- app.js
+++ app.js
@@ -54,3 +54,18 @@ thumbnails.forEach((thumbnail, index) => {
         showSlider();
     })
 })
+
+// Hamburger Menu
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+
+// Close menu when clicking a link
+document.querySelectorAll(".menu li a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+}));
